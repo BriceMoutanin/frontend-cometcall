e60837696d3ba52dd536d0a2739268e5cf48c7d5
Mathilde <ducros.mathilde@gmail.com>
2023-03-07 17:40:07 +0100

    faq

--- screens/FAQScreen.js
+++ screens/FAQScreen.js
@@ -2,7 +2,26 @@ import { StyleSheet, View, Image, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function FAQScreen({ navigation }) {
-  return <SafeAreaView></SafeAreaView>;
+  let FAQ = [
+    { question: "Comment on fait des cookies?", reponse: "Avec du chocolat" },
+    {
+      question: "Quelles sont les meilleures madeleines?",
+      reponse: "Les Bijoux evidement!",
+    },
+    {
+      question: "Fera-t-il beau demain? ",
+      reponse: "Vous n'êtes pas dans la bonne appli!!",
+    },
+  ];
+
+  let component = FAQ.map((value, i) => result);
+
+  return (
+    <SafeAreaView>
+      <Text style={styles.questions}>{component.question}</Text>
+      <Text style={styles.reponse}></Text>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
